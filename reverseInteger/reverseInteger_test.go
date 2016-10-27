3634b048e96d741fd5dc05174431332ce57f4287
You Donghai <youd@garena.com>
2016-10-27 16:26:26 +0700

    typo fix

--- reverseInteger/reverseInteger_test.go
+++ reverseInteger/reverseInteger_test.go
@@ -5,22 +5,22 @@ import "testing"
 func TestReverse(t *testing.T) {
 	r := Reverse(1234)
 	if r != 4321 {
-		t.Error("recerse 1234 failed")
+		t.Error("reverse 1234 failed")
 	}
 	r = Reverse(-410845)
 	if r != -548014 {
-		t.Error("recerse -410845 failed")
+		t.Error("reverse -410845 failed")
 	}
 	r = Reverse(-1)
 	if r != -1 {
-		t.Error("recerse -1 failed")
+		t.Error("reverse -1 failed")
 	}
 	r = Reverse(0)
 	if r != 0 {
-		t.Error("recerse 0 failed")
+		t.Error("reverse 0 failed")
 	}
 	r = Reverse(1534236469)
 	if r != 0 {
-		t.Error("recerse 0 failed")
+		t.Error("reverse 1534236469 failed")
 	}
 }
