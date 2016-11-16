6e65d417e5ac146f9872cc4b3ced810f07664594
You Donghai <youdonghai@gmail.com>
2016-11-16 09:55:40 +0700

    add test for three sum closest

--- threeSumClosest/threeSumClosest_test.go
+++ threeSumClosest/threeSumClosest_test.go
@@ -1,1 +1,10 @@
 package threeSumClosest
+
+import "testing"
+
+func TestThreeSumClosest(t *testing.T) {
+	r := ThreeSumClosest([]int{0, 0, 0, 1, 6, 7, 8, 4, -76, 3, -54, 7, 3, 5, 7, 8, 3, 6, 7, 3, 7, 5, 4}, 99)
+	if r != 23 {
+		t.Error("test error")
+	}
+}
